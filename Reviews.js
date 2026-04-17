f1dd48ea7e01fe95adeae6c8fce8eeddb7dbc151
yketu <157855738+yketu@users.noreply.github.com>
2026-04-17 08:48:41 -0600

    Add movieId field to Review schema

--- Reviews.js
+++ Reviews.js
@@ -5,7 +5,10 @@ mongoose.connect(process.env.DB);
 
 // Movie schema
 var ReviewSchema = new Schema({
-
+  movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' },
+  username: String,
+  review: String,
+  rating: { type: Number, min: 0, max: 5 }
 });
 
 // return the model
