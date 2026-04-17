d0fff53dbfab8db30026741a9649718b1de0349a
yketu <157855738+yketu@users.noreply.github.com>
2026-04-17 17:13:50 -0600

    Remove database connection from Reviews.js

--- Reviews.js
+++ Reviews.js
@@ -1,9 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect(process.env.DB);
-
-// Movie schema
 var ReviewSchema = new Schema({
   movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' },
   username: String,
@@ -11,5 +8,4 @@ var ReviewSchema = new Schema({
   rating: { type: Number, min: 0, max: 5 }
 });
 
-// return the model
 module.exports = mongoose.model('Review', ReviewSchema);
